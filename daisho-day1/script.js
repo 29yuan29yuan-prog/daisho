f96fe5cbb450dc2d3d1bccf2d30a42187a15b9d9
29yuan29yuan-prog <29yuan29yuan@gmail.com>
2025-10-30 14:06:08 +0900

    27

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -1,13 +1,13 @@
-const helloBtn = document.getElementByld("helloBtn");
-const message = document.getElementByld("message");
+const helloBtn = document.getElementById("helloBtn");
+const message = document.getElementById("message");
 
-helloBtn.addEventListener("click",() =>{
+helloBtn.addEventListener("click",()=>{
     const hour = new Date().getHours();
     let greeting = "こんにちは！";
     if(hour<12){
         greeting = "おはようございます！";
     }else if(hour>=18){
-        greeting="こんにちは！";
+        greeting="こんばんは！";
     }
     message.textContent=`${greeting} GitHub Pages の公開に成功しました！`;
 });
